453ee799abaa5eca0b928c75df2160eebad27ab2
Poludasarishashikumar <90147565+Poludasarishashikumar@users.noreply.github.com>
2023-04-16 14:15:54 +0530

    one way to show error by response

--- src/components/Loaderfetch2.js
+++ src/components/Loaderfetch2.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 function Loaderfetch2(props) {
-    const data=useLoaderData();
-    console.log(data);  //used to load the closest loader data
+    const data=useLoaderData();  //used to load the closest loader data
+    if(data.isError){
+       return <p>{data.message}</p>
+    }
+    console.log(data); 
     return (
         <div>
             {data.map((event)=>(
@@ -23,8 +26,11 @@ export  async function loader()
           // console.log(response);
           if (!response.ok) {
             // throw new Error('Something went wrong!');
+            return{isError:true,message:'Could not fetch data.'};//one way to show error
           }
     
-          const data = await response.json();
-          return data;
+        //   const data = await response.json();
+        //   return data;
+        console.log(response);
+        return response;
 }
